582af0f79df2bb517e90a487aa02c4171e691fc9
MauCoden95 <mauro.mig22@hotmail.com>
2023-02-06 16:59:36 -0300

    Primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Chicken World</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="http://localhost/chicken-world/resources/css/Styles.css">
+
+        <!--Font Awesome-->
+        <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="header-top">
+            <div class="center">
+                <a class="fas fa-shopping-cart"></a>
+                <a href="http://127.0.0.1:8000/login" class="fas fa-user"></a>
+            </div>
+        </div>
+        <header id="header">
+            <div class="center">
+                <img src="http://localhost/chicken-world/resources/Img/Logo1.png" alt="logo">
+            </div>
+            
+        </header>
+
+
+
+
+    </body>
+</html>
